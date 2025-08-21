784d29024aa0ba6bf8c1f487dd95f27cf87f7ea1
Nikkolas <minton.nikkolas@gmail.com>
2025-08-21 03:57:01 -0500

    instead of just the training set data, i put the full housing price dataset. I forgot that i scaled the values on the entire set, then split it into train/test in Collab

--- src/services/predict/instance.py
+++ src/services/predict/instance.py
@@ -5,7 +5,7 @@ from logging.logger_instance import logger
 from services.predict.impl import Predicter
 
 model=joblib.load("../../static/model.pkl")
-training_dataset=pd.read_csv("../../static/training_dataset.csv")
+training_dataset=pd.read_csv("../../static/housing_price_dataset.csv")
 scaler=StandardScaler()
 
 predicter = Predicter(logger=logger, model=model, dataset=training_dataset, scaler=scaler)
