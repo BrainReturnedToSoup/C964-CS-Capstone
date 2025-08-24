90a2df1f13225d68c590d6931508dff12b40a352
Nikkolas <minton.nikkolas@gmail.com>
2025-08-24 11:57:26 -0500

    installed pytest-order so i can define the order that tests execute in explicitly; important cross test deps

--- src/services/predicter/monte_carlo/impl_test.py
+++ src/services/predicter/monte_carlo/impl_test.py
@@ -7,8 +7,13 @@ def test_validate_constructor_args():
     valid_num_of_samples_min=5
     valid_num_of_samples_max=1000
     
-    # shoud not raise an exception
+    # should not raise an exception
     monte_carlo_predicter=MonteCarlo(logger=logger, noise_std=valid_noise_std, num_of_samples_min=valid_num_of_samples_min, num_of_samples_max=valid_num_of_samples_max)
+    
+    invalid_noise_std=-1
+    invalid_num_of_samples_min=0
+    invalid_num_of_samples_max_1=0
+    invalid_num_of_samples_max_2=3 # use in combination with valid_num_of_samples_min
     
 
 def test_validate_input():
